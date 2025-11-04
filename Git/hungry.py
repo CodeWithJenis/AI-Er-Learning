9e8f80d51f28e9d050f799cb279e750b7b474002
Jenish Ghimire <jenishghimire783@gmail.com>
2025-11-04 12:59:05 +0545

    Git Added

---
+++ Git/hungry.py
@@ -0,0 +1,12 @@
+hungry = input("are you hungry? = ")
+if hungry == "yes" or hungry == "Y" or hungry == "y":
+    print("eat food")
+    print("eat chocolate")
+    print("eat samosa")
+    print("eat pizza")
+    print("eat momo")
+else:
+    thirsty = input("are you thirsty? = ")
+    if thirsty == "yes":
+        print("drink water")
+        
